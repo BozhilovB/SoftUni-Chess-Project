e54a77a70b3bcbdcf9646e52eac5f22958e1a2dd
BozhilovB <bozhidarbozhilov97@gmail.com>
2024-11-24 20:56:05 +0200

    Initial commit: Created SoftUni Chess project Part 2

---
+++ SoftUniChess/SoftUniChess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoftUniChess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

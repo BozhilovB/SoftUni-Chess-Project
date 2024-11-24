3da92e6b41e9305f8055194721c8b22a787acd42
BozhilovB <bozhidarbozhilov97@gmail.com>
2024-11-24 20:50:41 +0200

    Clean slate

--- SoftUniChess/SoftUniChess/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SoftUniChess.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
